c49a65fa6568a9bc0feb81b2c0facf30e64942fb
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-13 18:38:24 +0000

    Hello World!

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+  app.get("/", function (req, res) {
+    res.send("Hello World!");
+  });
+});
