35494ca62c779dfc658e3be2f408511cb31ff69c
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-04-06 00:43:41 +0100

    More Details about Query Parameters

--- server.js
+++ server.js
@@ -53,17 +53,20 @@ http://localhost:3000/multiply?value1=4&value2=5
 
 This should return value of 20 */
 
+// app.get("/multiply", function (req, res) {
+//   let v1= req.query.value1;
+//   let v2 = req.query.value2;
+//   res.send(`You searched for value1: ${v1} and value2: ${v2}`);
+// });
+
 app.get("/multiply", function (req, res) {
-  let v1= req.query.value1;
+  let v1 = req.query.value1;
   let v2 = req.query.value2;
+  let v3 = req.query.value3;
+  let v4 = req.query;
   res.send(`You searched for value1: ${v1} and value2: ${v2}`);
   console.log(v1);
+  console.log(v2);
+  console.log(v3);
+  console.log(v4);
 });
-
-// app.get("/multiply", function (req, res) {
-//   let v1 = req.query;
-//   let v2 = req.query.value2;
-//   let v3 = req.query.value3;
-//   res.send(`You searched for value1: ${v1} and value2: ${v2}`);
-//   console.log(v1);
-// });
